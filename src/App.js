644d35e9f47a06d66f114577b8df6cb71fd343b7
Esteban <estebansolorzano27@gmail.com>
2019-08-16 23:42:37 -0500

    Add Github Corner

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import GithubCorner from 'react-github-corner';
 import Tetris from './components/Tetris';
 
 export default () => (
 	<div className="App">
 		<Tetris />
+		<GithubCorner href="https://github.com/estebansolo/TetrisGame" />
 	</div>
 );
